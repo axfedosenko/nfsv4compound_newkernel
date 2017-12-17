1a98aa4129570d26c0e08ce4b22bd015c93da3e7
Alexey Fedosenko <drifter228@gmail.com>
2017-12-17 09:36:00 +0700

    Fix errors

--- include/linux/nfs_fs.h
+++ include/linux/nfs_fs.h
@@ -355,7 +355,7 @@ extern void nfs_access_set_mask(struct nfs_access_entry *, u32);
 extern int nfs_permission(struct inode *, int);
 extern int nfs_open(struct inode *, struct file *);
 extern int nfs_attribute_cache_expired(struct inode *inode);
-extern bool nfs_need_revalidate_inode(struct inode *inode);
+// extern bool nfs_need_revalidate_inode(struct inode *inode);
 extern int nfs_revalidate_inode(struct nfs_server *server, struct inode *inode);
 extern int __nfs_revalidate_inode(struct nfs_server *, struct inode *);
 extern bool nfs_mapping_need_revalidate_inode(struct inode *inode);
